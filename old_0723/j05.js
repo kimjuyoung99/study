bad8124a6d11409b81f9c3b82203c632ab2247c7
Kimjuyoung <jjassb404@naver.com>
2024-07-29 16:36:43 +0900

    7/24 js문법 2차

--- old_0723/j05.js
+++ old_0723/j05.js
@@ -1,28 +1,28 @@
 // 표현식, 문, 리터럴
 
-// 산술연산자 + - * % / **
-console.log(2+3);
-console.log(11%2);
-console.log(5**2);
-console.log(Math.pow(5,2));
+// 산술연산자 + - * % /
+console.log(2 + 3);       // 5
+console.log(11 % 2);      // 1
+console.log(5 ** 2);      // 25
+console.log(Math.pow(5, 2)); // 25
 
 let text = "두개의" + "문자를 합";
 text = "1" + 1;
-console.log(text);
+console.log(text);        // '11'
 
 // 단항연산자 - + , !
 let a = 5;
 a = -a;
-console.log(a);//-5
+console.log(a);           // -5
 
-console.log(+true);
-console.log(+null);
-console.log(+"");
-console.log(+false);
-console.log(+"ttt");
-console.log(+undefined);
+console.log(+true);       // 1
+console.log(+null);       // 0
+console.log(+"");         // 0
+console.log(+false);      // 0
+console.log(+"ttt");      // NaN
+console.log(+undefined);  // NaN
 
-console.log(!!a);
+console.log(!!a);         // true (a는 -5로 true 값으로 평가)
 
 let aa = 1;
 aa = aa + 2;
@@ -35,16 +35,13 @@ aa--; // aa = aa - 1
 //aa++
 //++aa
 
-//대소관계 연산자 >, <. >=, <=
-console.log(2 > 3);
+// 대소관계 연산자 >, <. >=, <=
+console.log(2 > 3);       // false
 let result = 3 + (5 * 8) / 2;
 
-//동등 비교 관계 연산자
+// 동등 비교 관계 연산자
 // ==, !=, ===, !==
-let obj1 = {name : 'somy'}
-let obj2 = {name : "somy"}
-console.log(obj1 == obj2);
-console.log(obj1.name === obj2.name);
-
-
-// && || 
+let obj1 = { name: 'somy' }
+let obj2 = { name: "somy" }
+console.log(obj1 == obj2);          // false (객체는 참조가 다름)
+console.log(obj1.name === obj2.name); // true (문자열 값이 동일함)
---
+++ old_0724/j08.js
@@ -0,0 +1,99 @@
+// 객체
+let apple = {
+    0: "test1",
+    name: "somy",
+    ["hello bye"]: "안녕",
+    "age-num": () => {}
+};
+// key - 숫자, 문자, 문자열 'test-test', ['문자열'], 심볼
+// value = 원시값, 객체 (함수)
+
+// 객체를 만드는 방법
+// 1. 객체 리터럴로 만들기
+// 2. 클래스 객체로 만들기 new Object()
+// 3. Object.create()
+let sample = {};
+//대괄호 표기법은 언제 쓰이는가?
+console.log(apple.name);
+console.log(apple["hello bye"]);
+console.log(apple[0]);
+
+//객체에 속성 데이터 추가
+apple.emoji = '예쁜사과';
+console.log(apple);
+//객체 속성 삭제
+delete apple.emoji;
+console.log(apple);
+
+const obj1 = {
+    name: "개냥이",
+    age: 2,
+};
+
+console.log(obj1.name);
+//정적으로 할 수 있겠지만
+//동적으로 접글할 경우 -> 대괄호 사용!
+function getValue(object, key){
+    return object[key];//대괄호
+}
+console.log("getValue-- ", getValue(obj1, 'name'));
+
+
+const x = 10;
+const y = 20;
+// const coord = {x:x, y:y};
+const coord = {x,y};
+
+function makeObj(name,age){
+    return {name,age}
+}
+console.clear();
+const person = makeObj('samypage',5);
+console.log(person);//                  { name: 'samypage', age: 5 }
+
+const apple2 = {
+    name: "사과",
+    display : function(){
+        console.log(`함수 내부의 변수명 속성값 - ${this.name}`);
+    },
+};
+apple2.display();
+console.log(this);
+//this : 이벤트가 된 대상을 말하던지, 내 부모 객체를 말하던지!
+
+//$0 -> 콘솔에서 
+//브라우져에서 this : window
+//브라우져에서 global this : window
+//node 에서 this : 전역객체
+
+
+//생성자 함수
+function Fruite(name, color){
+    this.name = name,
+    this.color = color
+    this.display = () => {
+        console.log(`${this.name},${this.color}`);
+    };
+}
+//대문자로 시작
+const orange = new Fruite('오렌지','노랑');
+const banana = new Fruite('바나나','찐노랑');
+
+console.log(orange);
+console.log(banana);
+//생성자 함수를 만들었던 것을 class로 바꿔보기
+//객체지향 프로그래밍이란 것은 '템플릿'을 만드는 것.
+//class는 객체를 만들기 위한 템플릿으로 많이 쓰이고 있다.
+
+//class 로 그대로 만들어 보기
+class Fruite{
+    constructor(name, color){
+        this.name;
+        this.color;
+    }
+    //위의 constructor에 display를 넣을 수는 있다. this.display로
+    //근데 그러면 고정값이 되게 된다!
+    display = () => {
+        console.log(`${this.name}`, `${this.color}`);
+    }
+}
