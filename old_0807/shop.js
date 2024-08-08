724206f0b210e8e1ec6a8c0259bec26fc8942240
Kimjuyoung <jjassb404@naver.com>
2024-08-08 14:48:20 +0900

    08/08 카드 슬라이더

--- old_0807/shop.js
+++ old_0807/shop.js
@@ -1,25 +1,35 @@
+// HTML에서 id가 "itemInput"인 요소 선택
 const $itemInput = document.getElementById("itemInput");
+// HTML에서 id가 "addBtn"인 요소 선택
 const $addBtn = document.getElementById("addBtn");
+// HTML에서 id가 "items"인 요소 선택
 const $items = document.getElementById("items");
 
+// 로컬 스토리지에서 저장된 쇼핑목록 가져오고, 없으면 빈 배열로 초기화
 let shopList = JSON.parse(localStorage.getItem("shopList")) || [];
+// 현재 리스트에 있는 항목 중 가장 큰 id 찾아 그 다음 id 설정. 리스트가 비어있으면 0으로 초기화
 let id =
-  shopList.lenght > 0 ? Math.max(...shopList.map((item) => item.id)) + 1 : 0;
+  shopList.length > 0 ? Math.max(...shopList.map((item) => item.id)) + 1 : 0;
+
 // shopList.lenght > 0
 // Math.max(...shopList.map(item => item.id))+1
 
+// 페이지 로드 시 입력 필드에 포커스 줌
 $itemInput.focus();
 
+// shopList 배열을 localStorage에 저장하는 함수
 const saveToLoacalStorage = () => {
   localStorage.setItem("shopList", JSON.stringify(shopList));
 };
 
+// 새로운 쇼핑 항목 생성하는 함수
 //엔터키나 + 버튼을 눌렀을때, 한 번 load 되었을때 실행된다.
 const creatItem = (item) => {
   const $li = document.createElement("li");
-  // $li.classList.add('item')
+  // li 요소에 "item" 클래스 추가하고 항목의 id 설정
   $li.setAttribute("class", "item");
   $li.setAttribute("data-num", item.id);
+  // 항목의 텍스트와 삭제 버튼 추가
   $li.innerHTML = `
         <span>${item.text}</span>
         <i class="deletBtn fa-solid fa-trash-can" data-id="${item.id}"></i>
@@ -27,48 +37,58 @@ const creatItem = (item) => {
   return $li;
 };
 
+// 항목 추가하는 함수
 const onAdd = () => {
   const text = $itemInput.value;
+  // 입력 필드가 비어있으면 함수 종료하고 입력 필드에 포커스 줌
   if (text === "") {
     $itemInput.focus();
     return;
   }
-  // let stopList = [{id:id++, text: text},{},{},...]
+
+  // 새로운 아이템 객체 생성하고 shopList 배열에 추가
   const newItem = { id: id++, text: text };
   shopList.push(newItem);
-  console.log(shopList);
   saveToLoacalStorage();
 
+  // 새로운 항목 생성하고 리스트에 추가
   const item = creatItem(newItem);
   $items.appendChild(item);
+  // 새로 추가된 항목이 보이도록 스크롤
   item.scrollIntoView({ block: "end" });
 
+  // 입력 필드 초기화하고 포커스 줌
   $itemInput.value = "";
   $itemInput.focus();
 };
 
+// 추가 버튼 클릭하면 onAdd 함수 호출
 $addBtn.addEventListener("click", () => {
   onAdd();
 });
+
+// 입력 필드에서 엔터키 누르면 onAdd 함수 호출
 $itemInput.addEventListener("keyup", (e) => {
-  //   console.log(e.key === "Enter");
   if (e.key === "Enter") onAdd();
 });
 
+// items 리스트 클릭했을 때 발생하는 이벤트
 $items.addEventListener("click", (e) => {
-  console.log("target-- ", e.target.dataset.id);
-  console.log("currentTarget", e.currentTarget);
   let id = e.target.dataset.id;
+  // 클릭된 요소의 data-id 값 가져와 id 변수에 저장
   if (id) {
     id = parseInt(id);
+    // 해당 id를 가진 항목을 shopList 배열에서 제거
     shopList = shopList.filter((item) => item.id !== id);
     saveToLoacalStorage();
 
+    // DOM에서 해당 항목 찾아 제거
     const deleteItem = document.querySelector(`.item[data-num='${id}']`);
     deleteItem.remove();
   }
 });
 
+// 페이지 로드 시 shopList 배열에 있는 항목들을 DOM에 추가하는 함수
 const loadItems = () => {
   $items.innerHTML = "";
   shopList.forEach((item) => {
@@ -76,4 +96,5 @@ const loadItems = () => {
     $items.appendChild(itemElm);
   });
 };
+// 스크립트가 처음 로드될 때 loadItems 함수 호출
 loadItems();
---
+++ old_0808/index.js
@@ -0,0 +1,49 @@
+// btns
+const $show = document.getElementById("show");
+const $hide = document.getElementById("hide");
+const $clear = document.getElementById("clear");
+const $prev = document.getElementById("prev");
+const $next = document.getElementById("next");
+const $addCard = document.getElementById("add-card");
+
+const cardData = JSON.parse(localStorage.getItem("cards")) || [];
+console.log(cardData);
+
+// [새로운 카드를 등록 하세요]를 클릭하면 toggleClass(true) 실행
+// 게시물 관리를 위한 id 생성
+// 다음 버튼을 클릭하면 카드 이동 함수 실행
+// 카드 이동 함수 내에서 사용될 li 요소 리스트 const cardsEl = [];
+// 현재 카드를 인식하기 위한 currentActiveCard = 0 선언
+
+let id = 0;
+let cardEl = [];
+const createCard = (data, i) => {
+  const card = document.createElement("li");
+  card.classList.add("card");
+
+  if (i === 0) {
+    card.classList.add("active");
+  }
+
+  card.innerHTML = `
+    <div class="inner-card">
+      <div class="inner-card-front">
+        <p>${data.question}</p>
+      </div>
+      <div class="inner-card-back">
+        <p>${data.answer}</p>
+      </div>
+    </div>
+  `;
+  cardEl.push(card);
+  card.addEventListener("click", () => card.classList.toggle("show-card"));
+};
+
+$show.addEventListener("click",()=>toggleClass(true));
+$show.addEventListener("click",()=>toggleClass(false));
+document.addEventListener("click",(e)=>{
+    if(e.target.closet(".nolist")){
+        toggleClass(true);
+    }
+});
+
