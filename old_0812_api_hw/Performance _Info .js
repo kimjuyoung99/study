93a6f4429f98d8a0c0fd73b81a19aef4c992498b
Kimjuyoung <jjassb404@naver.com>
2024-08-12 17:35:28 +0900

    3차

--- old_0812_api_hw/Performance _Info .js
+++ old_0812_api_hw/Performance _Info .js
@@ -1,10 +1,7 @@
-const url = "http://api.prgrms-fe.xyz/events";
-// const url = "http://api.prgrms-fe.xyz/events/66b79f555ea334885261bb09";
-
+const url = "http://api.prgrms-fe.xyz/events?page=2&limit=16";
 
 fetch(url, {
     method: 'GET', // GET 요청
-
 })
 .then(response => {
     if (!response.ok) {
@@ -13,9 +10,26 @@ fetch(url, {
     return response.json(); 
 })
 .then(data => {
-    console.log(data);
+    const events = data.events; // 배열 데이터가 'events'라는 키에 있을 가능성
+    if (!Array.isArray(events)) {
+        throw new Error('Expected an array but got something else.');
+    }
+
+    const eventBox = document.querySelector('.eventBox');
+    
+    // 데이터를 순회하면서 박스 생성
+    events.forEach((event, index) => {
+        // 새로운 div 요소 생성
+        const box = document.createElement('div');
+        box.className = 'box';
+
+        // TITLE 값 삽입
+        box.textContent = event.TITLE;
+
+        // 박스를 eventBox에 추가
+        eventBox.appendChild(box);
+    });
 })
 .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
 });
-
